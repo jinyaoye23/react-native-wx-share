adff054ea68adeeefa1f9a24dca94393957b6f2a
shizhenzhen <kumbaya123@126.com>
2019-09-27 17:55:08 +0800

    android端集成微信sdk、修改包名为com.wxpay

--- android/src/main/java/com/wxpay/WxpayModulePackage.java
+++ android/src/main/java/com/wxpay/WxpayModulePackage.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package com.wxpay;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -8,12 +8,11 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
 
-public class WxShareModulePackage implements ReactPackage {
+public class WxpayModulePackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        return Arrays.<NativeModule>asList(new WxShareModuleModule(reactContext));
+        return Arrays.<NativeModule>asList(new WxpayModuleModule(reactContext));
     }
 
     @Override
