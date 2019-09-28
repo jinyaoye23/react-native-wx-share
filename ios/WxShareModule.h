2cfc42230950dde5f654c3aa8227d97941994cce
Stephen <jinyaoye23@126.com>
2019-09-28 17:18:24 +0800

    iOS 集成微信SDK

--- ios/WxShareModule.h
+++ ios/WxShareModule.h
@@ -1,5 +1,30 @@
-#import <React/RCTBridgeModule.h>
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface WxShareModule : NSObject <RCTBridgeModule>
+#import <React/RCTBridgeModule.h>
+#import "WXApi.h"
+
+// define share type constants
+#define RCTWXShareTypeNews @"news"
+#define RCTWXShareTypeThumbImageUrl @"thumbImage"
+#define RCTWXShareTypeImageUrl @"imageUrl"
+#define RCTWXShareTypeImageFile @"imageFile"
+#define RCTWXShareTypeImageResource @"imageResource"
+#define RCTWXShareTypeText @"text"
+#define RCTWXShareTypeVideo @"video"
+#define RCTWXShareTypeAudio @"audio"
+#define RCTWXShareTypeFile @"file"
+
+#define RCTWXShareType @"type"
+#define RCTWXShareTitle @"title"
+#define RCTWXShareDescription @"description"
+#define RCTWXShareWebpageUrl @"webpageUrl"
+#define RCTWXShareImageUrl @"imageUrl"
+
+#define RCTWXEventName @"WeChat_Resp"
+
+@interface WxShareModule : NSObject <RCTBridgeModule, WXApiDelegate>
+
+@property NSString* appId;
 
 @end
